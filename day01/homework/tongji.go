20fbe4c2de64497e0b2be90743ecadb70aaaf609
hi <wooluo@126.com>
2021-01-30 22:54:34 +0800

    homework
    课后作业，网上的答案不准确，还是自己做比较靠谱

---
+++ day01/homework/tongji.go
@@ -0,0 +1,32 @@
+/*
+*File  : tongji.go
+*Author: wooluo
+*Date  : 2021/1/30 22:44
+ */
+package main
+
+import "fmt"
+
+//作业内容，统计s1字符串中的英文有多少个
+//func main() {
+//	a := 0
+//	s1 := "hhhhhello沙河小王子"
+//	for _, i := range s1 {
+//		if i < 'z' { //包括字母内
+//			a++
+//		}
+//	}
+//	fmt.Println(a) //等于9，说明字符串中英文字母有9个
+//}
+
+//作业内容，统计s1字符串中的汉字有多少个
+func main() {
+	a := 0
+	s1 := "hhhhhello沙河小王子王子王子王子王子王子王子"
+	for _, i := range s1 {
+		if i > 'z' { //大于字母
+			a++
+		}
+	}
+	fmt.Println(a) //等于17，说明有17个汉字
+}
