35375d68e2ce8cdedc3f31cfa8dca7f628e9b9c1
hi <wooluo@126.com>
2021-01-31 12:31:27 +0800

    for循环

---
+++ day01/11for/main.go
@@ -0,0 +1,35 @@
+/*
+*File  : main.go
+*Author: wooluo
+*Date  : 2021/1/31 10:31
+ */
+package main
+
+import (
+	"fmt"
+)
+
+//for循环
+func main() {
+	//基本格式
+	//for i := 0; i < 1; i++ {
+	//	fmt.Println(i)
+	//}
+
+	//变种1
+	//var i = 5
+	//for ; i < 10; i++ {
+	//	fmt.Println("i")
+	//}
+
+	//变种2
+	//var i = 5
+	//for i < 10 {
+	//	fmt.Println(i)
+	//	i++ //从5开始累加，直到小于10的整数
+	//}
+
+	for {
+		fmt.Println("123") //听说你电脑很不错， 可以跑一下试试
+	}
+}
